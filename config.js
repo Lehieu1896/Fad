8c33d0176101e2b78a03a937fa3aef0c2b2b7730
Lehieu1896 <100954801+Lehieu1896@users.noreply.github.com>
2023-11-06 19:06:18 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Chúc mừng sinh nhật",
+    introTitle: ''Minh Ái',
+    introDesc: `Chúc mừng sinh nhật em :))).
+    Chúc em ngày càng thành công
+    luôn luôn vui vẻ
+    Bún trong tay tiền đầy túi :v`,
+    btnIntro: '^^HiHi^^',
+    title: 'You only live once, but if you do it right, once is enough',
+    desc: '欢迎你来到这地球日',
+    btnYes: ''Đoán xem gì đây nà',
+    btnNo: ''Đố em chọn được =))))',
+    question: 'Thích quà sinh nhật anh tặng không =)))',
+    btnReply: 'Gửi cho anh :v',
+    reply: 'Em thích lắm',
+    mess: 'Anh biết mà =))))',
+    messDesc: 'Ngủ ngon nhá. Muộn roài',
+    btnAccept: 'Ô sờ kê luôn',
+    messLink: 'https://www.facebook.com/messages/t/100004727201406'
+}
